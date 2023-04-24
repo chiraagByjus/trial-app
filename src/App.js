27cae2385e394a81787738c3fc9a1b8df5b9272c
Chiraag A <chiraag.a01@byjus.com>
2023-04-24 12:25:33 +0530

    addded lib configs

--- src/App.js
+++ src/App.js
@@ -1,24 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { CardWrapper, VideoPlayer } from "@chiraagbyjus/trial-library-dummy";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      {" "}
+      <CardWrapper width="200px" height="200px" backgroundColor="yellow">
+        HI
+      </CardWrapper>
+      <VideoPlayer
+        source="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4"
+        thumbnail="http://commondatastorage.googleapis.com/gtv-videos-bucket/sample/images/BigBuckBunny.jpg"
+        videoId="Big_Buck_Bunny_01"
+      />
+    </>
   );
 }
 
